7cc9d302fceacc8df204cf2f14b666e8b121b7ef
Sumanth-Palliboina <sumanthpalliboina143@gmail.com>
2026-09-11 15:47:37 +0530

    change theme with custom checkbox

---
+++ custom checkbox/index.js
@@ -0,0 +1,8 @@
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
